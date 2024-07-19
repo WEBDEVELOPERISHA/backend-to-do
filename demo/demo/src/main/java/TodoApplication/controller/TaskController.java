6a9bb20400fa51f4a56ace4d85ea5f99e9b0a06f
Isha Brijnarayan Tripathi <109479536+WEBDEVELOPERISHA@users.noreply.github.com>
2024-07-19 15:43:07 +0530

    Add files via upload

---
+++ demo/demo/src/main/java/TodoApplication/controller/TaskController.java
@@ -0,0 +1,57 @@
+package TodoApplication.controller;
+
+
+import TodoApplication.model.Task;
+import TodoApplication.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/tasks")
+
+public class TaskController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks(){
+        return taskRepository.findAll();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id){
+        Optional<Task> task = taskRepository.findById(id);
+        return task.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task taskDetails) {
+        Optional<Task> optionalTask = taskRepository.findById(id);
+        if (optionalTask.isPresent()) {
+            Task task = optionalTask.get();
+            task.setTitle(taskDetails.getTitle());
+            task.setDescription(taskDetails.getDescription());
+            task.setCompleted(taskDetails.isCompleted());
+            return ResponseEntity.ok(taskRepository.save(task));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        taskRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
---
+++ demo/demo/src/main/java/TodoApplication/repository/TaskRepository.java
@@ -0,0 +1,7 @@
+package TodoApplication.repository;
+
+import TodoApplication.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
